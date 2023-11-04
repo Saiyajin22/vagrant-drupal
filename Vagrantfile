c309b2b932ee78b52699a71a79a4038b6c2d2bf3
Saiyajin22 <tothzoltan0713@gmail.com>
2023-11-04 10:21:05 +0100

    running drupal with db

--- Vagrantfile
+++ Vagrantfile
@@ -12,9 +12,4 @@ Vagrant.configure("2") do |config|
 
     # Shell fájlok futtatása. Provision miatt csak első boot-nál!
     config.vm.provision "shell", path: "init.sh"
-
-    # Bekapcsoláskor automatikusan megcsinál valamit. (Pl Elindítja a drupalt)
-    # ns.trigger.after [:up, :reload] do |t|
-    #   t.run_remote = {inline: "cd express_example && forever start ./bin/www"}
-    # end
 end
--- init.sh
+++ init.sh
@@ -1,24 +1,21 @@
 sudo apt-get update
 yes Y | sudo apt-get install apache2
 yes Y | sudo apt-get install php8.1 libapache2-mod-php mcrypt php8.1-mysql mysql-server
-# sudo systemctl start apache2
 
-# install mysql-server and update mysql password
+# install mysql-server
 yes Y | sudo apt-get install mysql-server
-# sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'root';"
 
 # composer install
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 # installs extensions for drupal
-yes Y | sudo apt-get install zip unzip php8.1-zip php-curl php8.1-gd php8.1-xml php8.1-simplexml php8.1-dom
+yes Y | sudo apt-get install zip unzip php8.1-zip php-curl php8.1-gd php8.1-xml php8.1-simplexml php8.1-dom php8.1-mbstring
 
 # crate drupal project
 cd ../../var/www/html
 composer create-project drupal/recommended-project vagrant-drupal2
-# composer install --no-dev
-# composer update
+
 # Fix errors to enable creation of drupal project
 sudo chmod -R 777 vagrant-drupal2
 cd vagrant-drupal2/web/sites/default
@@ -29,7 +26,12 @@ sudo chmod -R 777 files
 
 # create database
 mysql -u root -e "CREATE DATABASE vagrant CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci";
-mysql -u root -e "SHOW DATABASES;";
+mysql -u root -e "SHOW DATABASES";
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'root';"
+
+# enable required things
+sudo phpenmod gd xml simplexml dom mbstring
+sudo a2enmod rewrite
 
 # configure apache for drupal
 # echo "ServerName localhost" | sudo tee /etc/apache2/apache2.conf
@@ -45,13 +47,5 @@ mysql -u root -e "SHOW DATABASES;";
 # </VirtualHost>
 # EOF
 
-# Enable the Drupal virtual host and disable the default Apache site.
-# sudo a2ensite drupal
-# sudo a2dissite 000-default
-
-# Clean up unnecessary files.
-# sudo rm /var/www/html/index.html
-
 # Start apache2 server
-# sudo systemctl stop apache2
 sudo apachectl start
